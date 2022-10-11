d620a6d3ec6d01c5615984cef1757705e69af04d
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2022-10-11 10:29:44 +0200

    Initial make

---
+++ js/main.js
@@ -0,0 +1,53 @@
+const articles = document.querySelectorAll('article')
+const resetBtn = document.querySelector('.reset-btn')
+
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+]
+
+let turn = true // true = cross
+
+resetBtn.addEventListener('click', resetGame)
+
+for (let i = 0; i < articles.length; i++) {
+    articles[i].addEventListener('click', onClick)
+}
+
+function onClick(data) {
+    let row = data.path[1].classList[0].charAt(4)
+    let col = data.path[1].classList[1].charAt(4)
+        if (board[row][col] == '') {
+        if (turn == true) {
+            data.target.attributes[0].nodeValue = "img/cross.png"
+            turn = false
+            board[row][col] = 'x'
+            checkWin('x')
+        } else {
+            data.target.attributes[0].nodeValue = "img/circle.png"
+            turn = true
+            board[row][col] = 'o'
+            checkWin('o')
+        }
+    }
+}
+
+function resetGame() {
+    for (let i = 0; i < articles.length; i++) {
+        articles[i].children[0].attributes[0].nodeValue = "img/trans.png"
+        turn = true
+        board = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ]
+    }
+}
+
+function checkWin(player) {
+    for (let i = 0; i <= 2; i++)
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
+            alert('win')
+        }
+}
