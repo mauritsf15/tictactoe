c966e2e61ed684d83d5dbd484b7ea5e349fd53db
mauritsf <57948944+mauritsf15@users.noreply.github.com>
2022-10-12 09:58:23 +0200

    Finish initial game

--- js/main.js
+++ js/main.js
@@ -15,10 +15,16 @@ for (let i = 0; i < articles.length; i++) {
     articles[i].addEventListener('click', onClick)
 }
 
+const dataBoard = [
+    [articles[0], articles[3], articles[6]],
+    [articles[1], articles[4], articles[7]],
+    [articles[2], articles[5], articles[8]]
+]
+
 function onClick(data) {
     let row = data.path[1].classList[0].charAt(4)
     let col = data.path[1].classList[1].charAt(4)
-        if (board[row][col] == '') {
+    if (board[row][col] == '') {
         if (turn == true) {
             data.target.attributes[0].nodeValue = "img/cross.png"
             turn = false
@@ -36,6 +42,7 @@ function onClick(data) {
 function resetGame() {
     for (let i = 0; i < articles.length; i++) {
         articles[i].children[0].attributes[0].nodeValue = "img/trans.png"
+        articles[i].style.backgroundColor = ''
         turn = true
         board = [
             ['', '', ''],
@@ -48,13 +55,33 @@ function resetGame() {
 function checkWin(player) {
     for (let i = 0; i <= 2; i++)
         if (board[i][0] == player && board[i][1] == player && board[i][2] == player) {
-            alert(player + ' win')
+            dataBoard[i][0].style.backgroundColor = '#42f563'
+            dataBoard[i][1].style.backgroundColor = '#42f563'
+            dataBoard[i][2].style.backgroundColor = '#42f563'
+            stopGame()
         } else if (board[0][i] == player && board[1][i] == player && board[2][i] == player) {
-            alert(player + ' win')
+            dataBoard[0][i].style.backgroundColor = '#42f563'
+            dataBoard[1][i].style.backgroundColor = '#42f563'
+            dataBoard[2][i].style.backgroundColor = '#42f563'
+            stopGame()
         }
     if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
-        alert (player + ' win')
+        dataBoard[0][0].style.backgroundColor = '#42f563'
+        dataBoard[1][1].style.backgroundColor = '#42f563'
+        dataBoard[2][2].style.backgroundColor = '#42f563'
+        stopGame()
     } else if (board [0][2] == player && board[1][1] == player && board[2][0] == player) {
-        alert (player + ' win')
+        dataBoard[0][2].style.backgroundColor = '#42f563'
+        dataBoard[1][1].style.backgroundColor = '#42f563'
+        dataBoard[2][0].style.backgroundColor = '#42f563'
+        stopGame()
     }
+}
+
+function stopGame() {
+    board = [
+        ['n', 'n', 'n'],
+        ['n', 'n', 'n'],
+        ['n', 'n', 'n']
+    ]
 }
